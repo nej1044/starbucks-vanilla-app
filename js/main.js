55ff35570822017bb402786b8d0d4c7b3f8188dc
nej1044 <nej1044@naver.com>
2023-01-15 01:16:26 +0900

    add: annimation

--- js/main.js
+++ js/main.js
@@ -30,3 +30,12 @@ window.addEventListener('scroll', _.throttle(() =>{
         })
     }
 }, 300))
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+
+fadeEls.forEach((fadeEl, index)=> {
+    gsap.to(fadeEl, 1, {
+        delay: .7 * (index+1),
+        opacity: 1
+    })
+})
