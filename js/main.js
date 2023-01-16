3c54eaf59640e71918ba89add99bdf8efc31a242
nej1044 <nej1044@naver.com>
2023-01-16 22:22:11 +0900

    add: 스크롤 애니메이션

--- js/main.js
+++ js/main.js
@@ -94,6 +94,18 @@ function floatingObject(selector, delay, size){
     })
 }
 
-floatingObject('.floating1', 1, 15)
-floatingObject('.floating2', .5, 15)
-floatingObject('.floating3', 1.5, 20)
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+
+spyEls.forEach((spyEl) => {
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl,
+            triggerHook: .8,
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller());
+})
