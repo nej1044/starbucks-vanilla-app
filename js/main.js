a87b791f10945cb372cf06d6291e36c1c539cb29
nej1044 <nej1044@naver.com>
2023-01-16 16:03:56 +0900

    add: 요소 슬라이드

--- js/main.js
+++ js/main.js
@@ -39,3 +39,40 @@ fadeEls.forEach((fadeEl, index)=> {
         opacity: 1
     })
 })
+
+new Swiper('.notice-line .swiper', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true,
+})
+
+new Swiper('.promotion .swiper', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+    },
+    loop: true,
+    pagination: {
+        el: '.promotion .swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+})
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function(){
+    isHidePromotion = !isHidePromotion
+    if(isHidePromotion){
+        promotionEl.classList.add('hide')
+    }else{
+        promotionEl.classList.remove('hide')
+    }
+})
