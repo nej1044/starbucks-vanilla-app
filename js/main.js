580dab38a38360dffd36496af9908bfcbb4f962c
nej1044 <nej1044@naver.com>
2023-01-17 16:20:14 +0900

    add: scroll top

--- js/main.js
+++ js/main.js
@@ -120,3 +120,6 @@ spyEls.forEach((spyEl) => {
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller());
 })
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
