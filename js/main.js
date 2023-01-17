39fcd396fcf61266b50cf693ca712295765f178c
nej1044 <nej1044@naver.com>
2023-01-17 13:25:39 +0900

    add: 다중 요소 슬라이드

--- js/main.js
+++ js/main.js
@@ -64,6 +64,17 @@ new Swiper('.promotion .swiper', {
     }
 })
 
+new Swiper('.awards .swiper', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
